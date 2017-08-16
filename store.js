ee51931fccf832b6210e2d006e23ca3ccc37c438
kvinash <kapilyadav93@gmail.com>
2017-08-16 15:09:02 +0530

    changes done

--- store.js
+++ store.js
@@ -7,7 +7,7 @@ import promise from "redux-promise-middleware";
 
 /**To initiate the react dev tool in chrome */
 import { composeWithDevTools } from 'redux-devtools-extension';
-const middleware = applyMiddleware( promise(),thunk, logger);
+const middleware = applyMiddleware(thunk, logger);
 var initia_store = [{'_id':123243,'name':'product123','price':'89'}]
 var store = createStore(reducers,{},composeWithDevTools(
   middleware,
